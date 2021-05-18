784804661c43c8441dfe71f756c0bca2976ff325
acarso02 <55151495+acarso02@users.noreply.github.com>
2021-05-18 14:46:41 -0400

    Add files via upload

---
+++ quantTradingAlgorithm/main.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+class ClassifyBar:
+    __near = 0.005         #Definition of "close enough" used in is_doji method
+    
+    def __init__(self, open, close, high, low, volume):
+        self.open = open
+        self.close = close
+        self.high = high
+        self.low = low
+        self.volume = volume
+        self.range = self.get_range()
+
+    def polarity(self):
+        """
+        Checks the polarity of the bar
+        returns: true if green or doji
+        """
+        return self.open <= self.close
+
+    def top_tail(self):
+        """
+        Checks the size of the top tail 
+        :returns: size of top tail, 0 if none (float)
+        """
+        if(self.open > self.close):
+            return self.high - self.open
+        else:
+            return self.high - self.close
+
+    def bottom_tail(self):
+        """
+        Checks the size of the bottom tail
+        :returns: size of the bottom tail, 0 if none (float)
+        """
+        if(self.open < self.close):
+            return self.open - self.low
+        else:
+            return self.close - self.low
+
+    def is_doji(self):
+        """
+        Checks if the open and close prices are the same 
+        :returns: True if prices are within range of eachother, false if not
+        """
+        if  abs(self.open - self.close) < ClassifyBar.__near:
+            return True
+
+        return False
+    
+    def get_range(self):
+        """
+        :returns: Range from OPEN to CLOSE
+        """
+        return abs(self.open - self.close)
+
+    def is_wide_range(self, recent_bars = []):
+        """
+        Checks if the current bar is of wide range
+        :params: recent_bars: list of n bars preceding the current bar
+        :returns: True if the current bar is larger than the last 3 bars and 
+                   two times the mean, false otherwise
+        """
+        mean = float(0.0)
+
+        for bar in recent_bars:
+            mean = mean + bar.get_range()
+        mean = mean / len(recent_bars)
+
+        if (self.get_range() >= recent_bars[len(recent_bars)- 1].get_range()
+        and self.get_range() >= recent_bars[len(recent_bars) - 2].get_range()
+        and self.get_range() >= recent_bars[len(recent_bars) - 3].get_range()
+        and self.get_range() >= 2.0 * mean):
+                return True
+
+        return False 
+
+    def is_volume_spike(self, recent_bars = []):
+        """
+        Checks if the current bar is a volume spike 
+        :params: recent_bars: list of n bars preceding the current bar
+        :returns: True if volume is twice the average of the n bars preceding
+                   it, false if not
+        """
+        mean = float(0.0)
+
+        for bar in recent_bars:
+            mean = mean + bar.volume
+        mean = mean / len(recent_bars)
+
+        if self.volume >= mean * 2:
+            return True
+
+        return False
+
+    def is_igniting_move(self, recent_bars = [], support = 0, resistance = 0):
+        """
+        Checks if the current bar is an igniting move 
+        :params: recent_bars: list of n bars preceding the current bar
+        :params: SRLevel: support or resistance level
+        :returns: True if criteria met, false if not
+        """
+        if (self.is_wide_range(recent_bars)
+        and self.is_volume_spike(recent_bars)):
+            if self.polarity and self.close > resistance:
+                return True
+            elif self.polarity == False and self.close < support:
+                return True
+        
+        return False
+
+
+    def is_ending_move(self, recent_bars = [], support = 0, resistance = 0):
+        """
+        Checks if the current bar is an ending move 
+        :params: recent_bars: list of n bars preceding the current bar
+        :params: SRLevel: support or resistance level
+        :returns: True if criteria met, false if not
+        """
+        if (self.is_wide_range(recent_bars)
+        and self.is_volume_spike(recent_bars)):
+            if self.polarity and self.close < resistance:
+                return True
+            elif self.polarity == False and self.close > support:
+                return True
+        
+        return False
+    
+# class RetrieveData:
+#     def init():
+#         pass
+        
+# class FormatData:
+#     def init():
+#         pass
+
+# class ConsolidateBar:
+#     def init():
+#         pass
+
+
+# Test Class for Simple Calculator functions to understand TDD/BDD
+class SimpleCalculator:
+    def add(self,*args):
+        return sum(args)
+        pass
+
+    def subtract(self,a,b,c=0):
+        return a-b-c
+        pass
---
+++ tests/test_main.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from quantTradingAlgorithm.main import SimpleCalculator
+
+from quantTradingAlgorithm.main import ClassifyBar
+import math as m
+# from quantTradingAlgorithm.main import RetrieveData
+# from quantTradingAlgorithm.main import FormatData
+# from quantTradingAlgorithm.main import ConsolidateBar
+
+################################
+# Unit Tests (ClassifyBar)
+################################
+
+def test_bar_is_green():
+    bar = ClassifyBar(4.0, 5.0, 6.2, 4.0, 50000)
+    result = bar.polarity()
+    assert result == True
+
+def test_bar_has_bottoming_tail():
+    # check for bottoming tail green bar
+    bar = ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000)
+    result = bar.bottom_tail()
+    assert m.isclose(result, 0.01)
+
+def test_bar_has_bottoming_tail2():
+    # check for bottoming tail red bar
+    bar = ClassifyBar(7.5, 3.6, 7.9, 3.2, 60000)
+    result = bar.bottom_tail()
+    assert m.isclose(result, 0.4)
+
+def test_bar_has_no_bottoming_tail():
+    # check for bottoming tail 
+    bar = ClassifyBar(6.8, 2.2, 6.8, 2.2, 50000)
+    result = bar.bottom_tail()
+    assert m.isclose(result, 0)
+
+def test_bar_has_topping_tail():
+    # check for topping tail
+    bar = ClassifyBar(4.0, 5.3, 5.31, 3.5, 50000)
+    result = bar.top_tail()
+    assert m.isclose(result, 0.01)
+
+def test_bar_has_no_topping_tail():
+    # check for topping tail green bar
+    bar = ClassifyBar(4.0, 5.3, 5.3, 3.5, 50000)
+    result = bar.top_tail()
+    assert result == 1
+
+def test_bar_is_doji():
+    bar = ClassifyBar(4.0, 4.002, 6.2, 3.5, 50000)
+    result = bar.is_doji()
+    assert result == True
+
+def test_bar_is_not_doji():
+    bar = ClassifyBar(4.0, 5.1, 6.2, 3.5, 50000)
+    result = bar.is_doji()
+    assert result == False
+
+def test_bar_is_wide_range():
+     # definition of wide range based on large difference between open/close, relative to surrounding bars
+     # larger than the last 3 bars and at least twice the average sized bar
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.58, 5.71, 5.10, 48000))
+
+    bar = ClassifyBar(5.9, 9.2, 9.2, 5.2, 66000)
+    result = bar.is_wide_range(bars)
+    assert result == True
+
+def test_bar_has_volume_spike():
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.58, 5.71, 5.10, 48000))
+
+    bar = ClassifyBar(5.9, 9.2, 9.2, 5.2, 100000)
+    result = bar.is_volume_spike(bars)    
+    assert result == True
+
+def test_bar_is_igniting_move():
+    # check for volume spike, wide range, and breaking through pivot (support/resistance) 
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.58, 5.71, 5.10, 48000))
+
+    bar = ClassifyBar(5.9, 9.2, 9.2, 5.2, 100000)
+    result = bar.is_igniting_move(bars, 8.5)
+    assert result == True
+
+def test_bar_is_not_igniting_move_volume():
+    # check for volume spike, wide range, and breaking through pivot (support/resistance) 
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.58, 5.71, 5.10, 48000))
+
+    bar = ClassifyBar(5.9, 9.2, 9.2, 5.2, 54000)
+    result = bar.is_igniting_move(bars, 8.50, 3.50)
+    assert result == False
+
+def test_bar_is_not_igniting_move_resistance():
+    # check for volume spike, wide range, and breaking through pivot (support/resistance) 
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.58, 5.71, 5.10, 48000))
+
+    bar = ClassifyBar(5.9, 9.2, 9.2, 5.2, 54000)
+    result = bar.is_igniting_move(bars, 10.00, 3.50)
+    assert result == False
+
+
+def test_bar_is_ending_move():
+    # check for volume spike, wide range, and failing at pivot (support/resistance)
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.13, 5.71, 5.10, 48000))
+    
+    bar = ClassifyBar(5.12, 2.50, 5.22, 2.50, 100000)
+    result = bar.is_ending_move(bars, 2.4, 8.5)
+    assert result == True
+
+def test_bar_is_ending_move():
+    # check for volume spike, wide range, and failing at pivot (support/resistance)
+    bars = []
+    bars.append(ClassifyBar(4.0, 5.3, 5.35, 3.99, 50000))
+    bars.append(ClassifyBar(5.3, 6.4, 7.6, 3.25, 45000))
+    bars.append(ClassifyBar(6.4, 5.7, 6.80, 5.6, 37000))
+    bars.append(ClassifyBar(5.6, 5.23, 5.71, 5.10, 54000))
+    bars.append(ClassifyBar(5.25, 5.13, 5.71, 5.10, 48000))
+    
+    bar = ClassifyBar(5.12, 2.50, 5.22, 2.50, 100000)
+    result = bar.is_ending_move(bars, 2.4, 8.5)
+    assert result == True
+
+################################
+# Unit Tests (RetrieveData)
+################################
+
+# def test_input_start_date_is_valid():
+#     assert result == True
+
+# def test_input_end_date_is_valid():
+#     assert result == True
+
+# def test_read_data_from_yahoo():
+#     assert result == []
+
+# def test_output_to_csv():
+#     assert result == []
+
+################################
+# Unit Tests (FormatData)
+################################
+
+# def test_map_dates():
+#     assert result == True
+
+# def test_drop_missing_values():
+#     assert result == True
+
+# def test_reset_index():
+#     assert result == True
+
+
+################################
+# Unit Tests (ConsolidateBar)
+################################
+
+# def test_resample_bar_data():
+#     assert result == True
+
+# def test_oversample_bar_data():
+#     assert result == True
+
+# def test_combine_two_bars():
+#     assert result == True
+
+################################
+# Unit Tests (ClassifyTrend)
+################################
+################################
+# Unit Tests (ClassifyMovingAverage)
+################################
+################################
+# Unit Tests (ClassifyBullish)
+################################
+################################
+# Unit Tests (ClassifyBearish)
+################################
+################################
+# Unit Tests (ClassifyRetracement)
+################################
+################################
+# Unit Tests (ClassifyMove)
+################################
+################################
+# Unit Tests (FindGap)
+################################
+################################
+# Unit Tests (FindResistance)
+################################
+################################
+# Unit Tests (FindSupport)
+################################
+################################
+# Unit Tests (DefinePattern)
+################################
+################################
+# Unit Tests (FindPattern)
+################################
+################################
+# Unit Tests (ClassifyBullish)
+################################
+################################
+
+# Simple Calculator Tests (for reference to better understand TDD/BDD)
+
+def test_add_two_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.add(4, 5)
+    assert result == 9
+
+def test_subtract_two_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.subtract(4, 5)
+    assert result == -1
+
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.add(4, 5, 6)
+    assert result == 15
+   
+def test_add_many_numbers():
+    calculator = SimpleCalculator()
+    numbers = range(100)
+    
+    result = calculator.add(*numbers)
+    assert result == 4950
+
